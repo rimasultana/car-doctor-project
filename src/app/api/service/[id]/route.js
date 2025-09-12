6fdb72e2c3aabe18565736784fc5b8a9629dbabe
rimasultana <rimasultana20225@gmail.com>
2025-09-12 22:59:20 +0600

    test

--- src/app/api/service/[id]/route.js
+++ src/app/api/service/[id]/route.js
@@ -5,11 +5,26 @@ import { NextResponse } from "next/server";
 import { ObjectId } from "mongodb"; 
 
 export const GET = async (req, { params }) => {
+  if (!params?.id) {
+    return NextResponse.json({ error: "ID is required" }, { status: 400 });
+  }
+
+  let objectId;
+  try {
+    objectId = new ObjectId(params.id);
+  } catch (err) {
+    return NextResponse.json({ error: "Invalid ID" }, { status: 400 });
+  }
+
   const servicesCollection = await dbConnect(
     collectionNameObj.servicesCollection
   );
-  const data = await servicesCollection.findOne({
-    _id: new ObjectId(params.id),
-  });
+
+  const data = await servicesCollection.findOne({ _id: objectId });
+
+  if (!data) {
+    return NextResponse.json({ error: "Service not found" }, { status: 404 });
+  }
+
   return NextResponse.json(data);
 };
